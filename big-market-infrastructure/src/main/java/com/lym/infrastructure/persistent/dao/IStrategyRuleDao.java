65ea47af76be1046ff93ebdcc60f2225b4d4d056
Lym <10112818+lym_no_girlfriend@user.noreply.gitee.com>
2026-02-21 21:45:15 +0800

    feat:orm 基础层

---
+++ big-market-infrastructure/src/main/java/com/lym/infrastructure/persistent/dao/IStrategyRuleDao.java
@@ -0,0 +1,19 @@
+package com.lym.infrastructure.persistent.dao;
+
+
+import com.lym.infrastructure.persistent.po.StrategyRule;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 策略规则 DAO
+ * @create 2023-12-16 13:25
+ */
+@Mapper
+public interface IStrategyRuleDao {
+
+    List<StrategyRule> queryStrategyRuleList();
+
+}
