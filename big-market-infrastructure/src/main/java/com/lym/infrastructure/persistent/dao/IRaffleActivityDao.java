fa3f677c3afd47914c67f76d68770d9e809a8e09
Lym <10112818+lym_no_girlfriend@user.noreply.gitee.com>
2026-03-03 17:44:27 +0800

    feat:引入分库组件

---
+++ big-market-infrastructure/src/main/java/com/lym/infrastructure/persistent/dao/IRaffleActivityDao.java
@@ -0,0 +1,17 @@
+package com.lym.infrastructure.persistent.dao;
+
+
+import com.lym.infrastructure.persistent.po.RaffleActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 抽奖活动表Dao
+ * @create 2024-03-09 10:04
+ */
+@Mapper
+public interface IRaffleActivityDao {
+
+    RaffleActivity queryRaffleActivityByActivityId(Long activityId);
+
+}
